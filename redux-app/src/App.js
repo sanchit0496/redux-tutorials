75b59c209863a203faff9068ca79fcce1adaad82
Sanchit <sanchit0496@gmail.com>
2021-06-27 01:33:46 +0530

    updating

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { incNumber, decNumber } from './actions/index.js'
 
 const App = () => {
+    const myState = useSelector((state) => state.changeTheNumber);
+    const dispatch = useDispatch();
+
     return (
         <div>
             <div className="container">
@@ -8,9 +13,9 @@ const App = () => {
                 <h4>Using React and Redux</h4>
 
                 <div className="quantity">
-                    <a href="" className="quantity_minus" title = "Decrement"> <span> - </span> </a>
-                    <input name = "quantity" type="text" class = "quantity_input" value = "0"/>
-                    <a href="" className="quantity_plus" title = "Increment"> <span> + </span> </a>
+                    <a href="" className="quantity_minus" title = "Decrement" onClick = {() => dispatch(decNumber())}> <span> - </span> </a>
+                    <input name = "quantity" type="text" className = "quantity_input" value = {myState}/>
+                    <a href="" className="quantity_plus" title = "Increment" onClick = {() => dispatch(incNumber())}> <span> + </span> </a>
 
                 </div>
 
--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import store from './store.js';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+<Provider store = {store}>
+<App />
+</Provider>
+, document.getElementById('root'))

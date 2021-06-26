49580eb492699ffdd4b7f1c455171355388f53d7
Sanchit <sanchit0496@gmail.com>
2021-06-27 00:34:56 +0530

    adding jsx

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -3,7 +3,18 @@ import React from 'react'
 const App = () => {
     return (
         <div>
-            App
+            <div className="container">
+                <h2>Increment and Decrement Counter</h2>
+                <h4>Using React and Redux</h4>
+
+                <div className="quantity">
+                    <a href="" className="quantity_minus" title = "Decrement"> <span> - </span> </a>
+                    <input name = "quantity" type="text" class = "quantity_input" value = "0"/>
+                    <a href="" className="quantity_plus" title = "Increment"> <span> + </span> </a>
+
+                </div>
+
+            </div>
         </div>
     )
 }
